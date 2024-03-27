a1185b3339c4624f51daf611e2b29662c15c4e4f
Bastian Kiefer <bastian.kiefer@endress.com>
2024-03-27 10:25:22 +0100

    Working and Runnning version

--- main.py
+++ main.py
@@ -24,7 +24,24 @@ def readData()->dict:
 def genNewSerial()->int:
     lastID = max(item["id"] for item in runningData["rows"])
     return  lastID + 1
-    
+
+def updateAvailability(input, tableRef, setting:bool):
+    if type(input) == str:
+        for row in runningData["rows"]:
+            if row["id"] == int(input):
+                row["flagged"] = setting
+            else:
+                pass
+    elif type(input) == list:
+
+        for inp in input:
+            for row in runningData["rows"]:
+                if row["id"] == int(inp['id']):
+                    row["flagged"] = setting
+                else:
+                    pass
+    saveData(runningData)
+    tableRef.update_rows(runningData['rows'])
 
 runningData = readData()
 
@@ -32,12 +49,12 @@ runningData = readData()
 def editorView():
     with ui.table(columns=runningData['columns'], rows=runningData['rows'], selection='multiple').classes('w-full') as table:
         with table.add_slot('top-left'):
-            with ui.input(placeholder='Search').props('type=search').bind_value(table, 'filter').add_slot('append'):
+            inputRef = ui.input(placeholder='Search').props('type=search').bind_value(table, 'filter').on('keydown.enter',lambda: (updateAvailability(inputRef.value, table, False),inputRef.set_value(None)))
+            with inputRef.add_slot("append"):
                 ui.icon('search')
         with table.add_slot('top-right'):
-            ui.button('Remove', on_click=lambda: (table.remove_rows(*table.selected), 
-                    saveData(runningData))) \
-                .bind_enabled_from(table, 'selected', backward=lambda val: bool(val))
+            ui.button('Refilled', on_click=lambda: updateAvailability(table.selected, table, False)).bind_enabled_from(table, 'selected', backward=lambda val: bool(val))
+            ui.button('Remove', on_click=lambda: (table.remove_rows(*table.selected),saveData(runningData))).bind_enabled_from(table, 'selected', backward=lambda val: bool(val))
             with ui.link(target=normalView):
                 ui.button('Close View')
 
@@ -57,29 +74,25 @@ def editorView():
         table.set_fullscreen(True)
         table.add_slot('body-cell-flag', '''
             <q-td key="flagged" :props="props">
-                <q-badge :color="props.value < True ? 'red' : 'green'">
+                <q-badge :color="props.value < true ? 'green' : 'red'">
                     {{ props.value }}
                 </q-badge>
             </q-td>
             ''')
-
-def scannerInput(input, tableRef):
-    print("8 Inputs")
-    #TODO flag it
         
 @ui.page('/')
 def normalView():
     with ui.table(columns=runningData['columns'], rows=runningData['rows']).classes('w-full') as table:
         with table.add_slot('top-left'):
-            inputRef = None
-            inputRef = ui.input(placeholder='Scanner', on_change=lambda v: (scannerInput(v.value, table), inputRef.set_value(None),inputRef.update(), ui.notify("Scanned")) if v.value != None and len(v.value)==8 else inputRef.update()).bind_value(table, 'filter')
+            inp = None
+            inputRef = ui.input(placeholder='Scanner').props('autofocus').bind_value(table, 'filter').on('keydown.enter',lambda: (updateAvailability(inputRef.value, table, True),inputRef.set_value(None))) #.on('keydown.enter', lambda v: (updateAvailability(v.value, table, True), inputRef.set_value(None),inputRef.update(), ui.notify("Scanned")) if v.value != None and len(v.value)==8 else (inputRef.update()))
         with table.add_slot('top-right'):
             with ui.link(target=editorView):
                 ui.button('Editor View')
         table.set_fullscreen(True)
         table.add_slot('body-cell-flag', '''
             <q-td key="flagged" :props="props">
-                <q-badge :color="props.value < True ? 'red' : 'green'">
+                <q-badge :color="props.value < true ? 'green' : 'red'">
                     {{ props.value }}
                 </q-badge>
             </q-td>
